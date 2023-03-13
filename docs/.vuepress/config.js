acf4a8928931cf10ea10c58f4bbe763915aa55c8
coder <17629065820@163.com>
2023-03-13 16:28:57 +0800

    添加jsdelivr cdn

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,9 +1,9 @@
 const dsalg = require('../dsalg')
+const path = require('path')
 
 module.exports = {
     title: '数据结构与算法',
     base: '/note/',
-    // base: '.',  
     dest: 'build/.vuepress/dist',
     markdown: {
         lineNumbers: true
@@ -12,10 +12,35 @@ module.exports = {
         sidebar: 'auto',
         logo: '/hero.jpg',
         lastUpdated: 'Last Updated',
-        editLinkText: '帮助我们改善此页面！',   
-        // smoothScroll: true,
+        editLinkText: '帮助我们改善此页面！',
         sidebar: {
             '/dsalg/': dsalg()
+        }
+    },
+    configureWebpack:() => {
+        const NODE_ENV = process.env.NODE_ENV
+        //判断是否是生产环境
+        if(NODE_ENV === 'production'){
+          return {
+            output: {
+              publicPath: 'https://cdn.jsdelivr.net/gh/coder-xiaozhu/note@gh-pages/'
+            },
+            resolve: {
+              //配置路径别名
+              alias: {
+                'public': path.resolve(__dirname, './public') 
+              }
+            }
+          }
+        }else{
+          return {
+            resolve: {
+              //配置路径别名
+              alias: {
+                'public': path.resolve(__dirname, './public') 
+              }
+            }
+          }
         }
     },
     head: [
